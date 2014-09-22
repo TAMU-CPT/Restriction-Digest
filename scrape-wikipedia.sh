4cecc0e141903e5e490feafe0d4725b9371a21a3
Stephen Crosby <stcrosby@gmail.com>
2014-09-21 23:37:58 -0500

    Scraper for loop

---
+++ scrape-wikipedia.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in {'A','Ba-Bc','Bd-Bp','Bsa-Bso','Bsp','Bsr-Bss','Bst','Bsu-Bv','C','D',$
+do
+    wget http://http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting$
+    sleep 1;
+done;
