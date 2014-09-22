dacce197b1e9850778bdaf0469c489927487fe66
Stephen Crosby <stcrosby@gmail.com>
2014-09-22 00:14:02 -0500

    Appears to work

--- scrape-wikipedia.sh
+++ scrape-wikipedia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {'A','C','D','E','F','G','H','I','K','L','M','N','O','P','R','S','T','U','V','X','Y','Z','Ba-Bc','Bd-Bp','Bsa-Bso','Bsp','Bsr-Bss','Bst','Bsu-Bv'};
+for i in A C-D E-F G-K L-N O-R S T-Z Ba-Bc Bd-Bp Bsa-Bso Bsp-Bss Bst-Bv
 do
-    wget http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting_sites:$i -O $i.html;
-    sleep 1;
+   wget  http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting_sites:_$i  -O $i.html;
+ sleep 1;
 done;
