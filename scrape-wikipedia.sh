798bf499b35d77914bd4f52e0dd7bfc8e2c24b72
Stephen Crosby <stcrosby@gmail.com>
2014-09-21 23:48:08 -0500

    Fixed copy/paste issue

--- scrape-wikipedia.sh
+++ scrape-wikipedia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {'A','Ba-Bc','Bd-Bp','Bsa-Bso','Bsp','Bsr-Bss','Bst','Bsu-Bv','C','D',$
+for i in {'A','C','D','E','F','G','H','I','K','L','M','N','O','P','R','S','T','U','V','X','Y','Z','Ba-Bc','Bd-Bp','Bsa-Bso','Bsp','Bsr-Bss','Bst','Bsu-Bv'};
 do
-    wget http://http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting$
+    wget http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting_sites:$i -O $i.html;
     sleep 1;
 done;
