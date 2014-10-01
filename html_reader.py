9f94d1aaa2da9dc6ee670f93d4169fd3a4e866d8
Stephen Crosby <stcrosby@gmail.com>
2014-09-30 19:38:25 -0500

    For a given html file, this returns three lists. The first list is of the enzyme names, the second list is the recognition sequence, and the third list is the specific cut made by the enzyme.

--- html_reader.py
+++ html_reader.py
@@ -18,15 +18,61 @@ with open(sys.argv[1],'r') as file:
 #    while 29+ inc <=len(new_list):
 #        print new_list[23+inc:29+inc]
 #        inc += 6 
+#    iterator = 1
+#    dummy = 1
+#    soup = BeautifulSoup(file)
+#    list = soup.find_all('table',border='1',limit=1)
+#    for element in list:
+#        sublist = element.find_all('tr',recursive=False)
+#	for subelement in sublist:
+#            subsublist = subelement.find_all('td',recursive=False)
+#            for element in subsublist:
+#                if element.table is None:
+#		    print iterator,element.get_text()
+#                    iterator+=1
+#    iterator = 1
+#    dummy = 1
+#    soup = BeautifulSoup(file)
+#    list = soup.find_all('table',border='1',limit=1)
+#    for element in list:
+#        sublist = element.find_all('tr',recursive=False)
+#        for subelement in sublist:
+#            subsublist = subelement.find_all('td',recursive=False)
+#            for element in subsublist:
+#                if element.table is None:
+#                    print iterator,element.get_text()
+#                    iterator+=1
+#                else:
+#                    print iterator
+#                    iterator+=1
+
+
+
     iterator = 1
     dummy = 1
+    enzyme_list = []
+    cut_list_1 = []
+    cut_list_2 = []
     soup = BeautifulSoup(file)
     list = soup.find_all('table',border='1',limit=1)
     for element in list:
         sublist = element.find_all('tr',recursive=False)
-	for subelement in sublist:
+        for subelement in sublist:
             subsublist = subelement.find_all('td',recursive=False)
             for element in subsublist:
-                if element.table is None:
-		    print iterator,element.get_text()
-                    iterator+=1
+                if iterator%6==1:
+                    enzyme_list+=[element.get_text()]
+                elif iterator%6==4:
+                    cut_list_1+=[element.get_text()]
+                elif iterator%6==5:
+                    cut_list_2+=[element.get_text()]
+                iterator+=1
+    print enzyme_list
+    print cut_list_1
+    print cut_list_2
+#list = soup.find_all('table',border='1',limit=1)
+#for element in list:
+#    sublist = element.find_all('tr',recursive=False)
+#    for subelement in sublist:
+#        subsublist = subelement.find_all('td',recursive=False)
+#print subsublist[0:20]
