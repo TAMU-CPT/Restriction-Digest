703468922875f76da7e260773e13dd469b945dfd
Stephen Crosby <stcrosby@gmail.com>
2014-10-08 14:32:22 -0500

    Should strip all tables off page, even those which have some rows missing the last cell. Still cleaning up text output.

--- html_reader.py
+++ html_reader.py
@@ -1,82 +1,37 @@
 from bs4 import BeautifulSoup
 import sys
 with open(sys.argv[1],'r') as file:
-
-#    soup = BeautifulSoup(file)
-#    list = soup.find_all('td')
-#    new_list = []
-#    new_new_list = []
-#    for element in list:
-       # Temp_Soup = BeautifulSoup(str(element))
-       # Temp_List = Temp_Soup.find_all('td')
-       # for e in Temp_List:
-           # new_list += [BeautifulSoup(str(e)).get_text()]
-    #Desired Table Headers, which should be the same on all the pages because they have the same format
-#        new_list += [BeautifulSoup(str(element)).get_text()]
-#    print new_list[17:23]
-#    inc = 0
-#    while 29+ inc <=len(new_list):
-#        print new_list[23+inc:29+inc]
-#        inc += 6 
-#    iterator = 1
-#    dummy = 1
-#    soup = BeautifulSoup(file)
-#    list = soup.find_all('table',border='1',limit=1)
-#    for element in list:
-#        sublist = element.find_all('tr',recursive=False)
-#	for subelement in sublist:
-#            subsublist = subelement.find_all('td',recursive=False)
-#            for element in subsublist:
-#                if element.table is None:
-#		    print iterator,element.get_text()
-#                    iterator+=1
-#    iterator = 1
-#    dummy = 1
-#    soup = BeautifulSoup(file)
-#    list = soup.find_all('table',border='1',limit=1)
-#    for element in list:
-#        sublist = element.find_all('tr',recursive=False)
-#        for subelement in sublist:
-#            subsublist = subelement.find_all('td',recursive=False)
-#            for element in subsublist:
-#                if element.table is None:
-#                    print iterator,element.get_text()
-#                    iterator+=1
-#                else:
-#                    print iterator
-#                    iterator+=1
-
-
-    i = 1
+    i = 0
     iterator = 1
-    dummy = 1
+    j = 0
     enzyme_list = []
     cut_list_1 = []
     cut_list_2 = []
-    soup = BeautifulSoup(file)
-    list = soup.find_all('table',border='1',limit=1)
-    for element in list:
-        sublist = element.find_all('tr',recursive=False)
-        for subelement in sublist:
-            subsublist = subelement.find_all('td',recursive=False)
-            for element in subsublist:
-                if iterator%6==1:
-                    enzyme_list+=[element.get_text()]
-                elif iterator%6==4:
-                    cut_list_1+=[element.get_text()]
-                elif iterator%6==5:
-                    cut_list_2+=[element.get_text()]
+    page = BeautifulSoup(file)
+    list_of_tables = page.find_all('table',border='1')
+    for table  in list_of_tables:
+        list_of_rows  = table.find_all('tr',recursive=False)
+        for row  in list_of_rows:
+            list_of_cells  = row.find_all('td',recursive=False)
+            for cell in list_of_cells:
+                if iterator == 1 and cell.get_text()!= 'Enzyme':
+                    enzyme_list += [cell.get_text()]
+                elif iterator == 4 and cell.get_text()!='Recognition sequence':
+                    cut_list_1 += [cell.get_text()]
+                elif iterator == 5 and cell.get_text()!='Cut':
+                    cut_list_2 += [cell.get_text()]
                 iterator+=1
-with open('data.txt','a') as f:
+            iterator = 1
+    print enzyme_list
+'''
+Cleans up the text and writes the list to a file.
+'''
+with open('test.txt','r+') as f:
     while i < len(enzyme_list):
-        string = (enzyme_list[i].replace(u'\xa0',u' ')+' '+cut_list_1[i].replace(u'\xa0',u' ').replace(u'\n3',u' ')+' '+cut_list_2[i].replace(u'\xa0',u' ').replace(u'\n3',u' '))
+        string = enzyme_list[i]+' ['+cut_list_1[i].replace('\n','') + '] ['+cut_list_2[i].replace(u'\xa0','').replace('\n','') + ']'
+        #.replace(u'\xa0',u' ')+' '+cut_list_1[i].replace(u'\xa0',u' ').replace(u'\n3',u' ')+' '+cut_list_2[i].replace(u'\xa0',u' ').replace(u'\n3',u' '))
         f.write(string)
         f.write('\n')
         i+=1
-  
-#list = soup.find_all('table',border='1',limit=1)
-#for element in list:
-#    sublist = element.find_all('tr',recursive=False)
-#    for subelement in sublist:
-#        subsublist = subelement.find_all('td',recursive=False)
-#print subsublist[0:20]
+    print f.readlines()
+
