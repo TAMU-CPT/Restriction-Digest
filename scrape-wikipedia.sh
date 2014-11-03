7c3657b7ebc2e536fff43f22042f46c253678a5c
Eric Rasche <rasche.eric@yandex.ru>
2014-11-03 10:05:29 -0600

    Moved to a data folder

--- scrape-wikipedia.sh
+++ scrape-wikipedia.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+mkdir -p data
 for i in A C-D E-F G-K L-N O-R S T-Z Ba-Bc Bd-Bp Bsa-Bso Bsp-Bss Bst-Bv
 do
-   wget  http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting_sites:_$i  -O $i.html;
+   wget  http://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting_sites:_$i  -O data/$i.html;
  sleep 1;
 done;
