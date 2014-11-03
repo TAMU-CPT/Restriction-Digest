bcc06bfe5dc7c03dbd2f353b98714c9f28d032c2
Eric Rasche <rasche.eric@yandex.ru>
2014-11-03 10:22:46 -0600

    Converted to use yaml and standard data structures

--- html_reader.py
+++ html_reader.py
@@ -2,44 +2,36 @@ from bs4 import BeautifulSoup
 import sys
 import glob
 import os
+import yaml
+import string
 
+def f(s):
+    # Remove non-printable characters/bad characters, and encode down to ascii.
+    # Probably only /really/ need to do second step, but just in case.
+    return ''.join(c for c in s if c in string.printable).encode('ascii', 'ignore')
+
+enzyme_list = []
 for i in glob.glob(os.path.join(sys.argv[1], '*')):
+    print i
     with open(i,'r') as file:
-        i = 0
-        iterator = 1
-        j = 0
-        enzyme_list = []
-        cut_list_1 = []
-        cut_list_2 = []
         page = BeautifulSoup(file)
         list_of_tables = page.find_all('table',border='1')
         for table  in list_of_tables:
             list_of_rows  = table.find_all('tr',recursive=False)
-            for row  in list_of_rows:
-                list_of_cells  = row.find_all('td',recursive=False)
-                for cell in list_of_cells:
-                    if iterator == 1 and cell.get_text()!= 'Enzyme':
-                        enzyme_list += [cell.get_text()]
-                    elif iterator == 4 and cell.get_text()!='Recognition sequence':
-                        cut_list_1 += [cell.get_text()]
-                    elif iterator == 5 and cell.get_text()!='Cut':
-                        cut_list_2 += [cell.get_text()]
-                    iterator+=1
-                iterator = 1
-    # print enzyme_list
-    '''
-    Cleans up the text and writes the list to a file.
-    '''
-    with open('test.txt','a') as f:
-        while i < len(enzyme_list):
-            enzyme_string  = enzyme_list[i]
-            cut_string_1 =' ['+cut_list_1[i].replace('\n','') + ']'
-            cut_string_2 =' ['+cut_list_2[i].replace(u'\xa0','') + ']'
-            cut_string_2 = cut_string_2.replace('\n','')
-            string = enzyme_string+cut_string_2+'\n'
-            #.replace(u'\xa0',u' ')+' '+cut_list_1[i].replace(u'\xa0',u' ').replace(u'\n3',u' ')+' '+cut_list_2[i].replace(u'\xa0',u' ').replace(u'\n3',u' '))
-            f.write(string)
-            i+=1
-
-
-
+            # Skip first line, header
+            for row  in list_of_rows[1:]:
+                list_of_cells = row.find_all('td',recursive=False)
+                # Cells are either newline separated, or comma separated. We
+                # need to fix the string data, then split on '\n', as we would
+                # rather have recognition_sequence and cut_site be two objects
+                # in a list, rather than a single string.
+                enzyme_list.append({
+                    'enzyme': f(list_of_cells[0].get_text()),
+                    'recognition_sequence': f(list_of_cells[3].get_text()).split('\n'),
+                    'cut': f(list_of_cells[4].get_text()).split('\n'),
+                    # These are comma separated, and surrounded by whitespace,
+                    # thanks wikipeda. So, run .strip() on all to clean them up
+                    'isoscizomers': [e.strip() for e in f(list_of_cells[5].get_text()).split(',')],
+                })
+with open('enzyme_data.yaml', 'w') as handle:
+    handle.write(yaml.dump(enzyme_list, default_flow_style=False))
