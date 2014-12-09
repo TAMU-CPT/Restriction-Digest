b16e4a877e1b15247aee5699f22ff99e37a9e19c
Stephen Crosby <stcrosby@gmail.com>
2014-12-09 11:37:48 -0600

    Should generate circular digest graphic.

--- dnadigest/__init__.py
+++ dnadigest/__init__.py
@@ -3,7 +3,8 @@ import yaml
 
 
 class Dnadigest():
-
+    def __init__(self):
+        self.data = ''
     def get_dict(self, data_file):
         with open(data_file) as handle:
             data_structure = yaml.load(handle)
@@ -72,40 +73,44 @@ class Dnadigest():
             working_seq = sequence*2
             start = rec_seq.search(str(sequence))
             if rec_seq.search(working_seq) is not None and len(rec_seq.findall(working_seq))<=1:
-                return working_seq[rec_seq.search(working_seq).start()+recog_nucl_index:len(sequence)]+working_seq[0:rec_seq.search(working_seq).start()+recog_nucl_index],'END OF SEQUENCE','linear'
+                return working_seq[rec_seq.search(working_seq).start()+recog_nucl_index:len(sequence)]+working_seq[0:rec_seq.search(working_seq).start()+recog_nucl_index],'END OF SEQUENCE','linear',rec_seq.search(working_seq).start()+recog_nucl_index
             else:
-                return sequence,'END OF SEQUENCE','circular'
+                return sequence,'END OF SEQUENCE','circular',''
         if len(rec_seq.findall(str(sequence)))== 1 and status == 'circular':
             working_seq = sequence*2
             start = rec_seq.search(str(sequence))
             if rec_seq.search(working_seq) is not None:
-                return working_seq[rec_seq.search(working_seq).start()+recog_nucl_index:len(sequence)]+working_seq[0:rec_seq.search(working_seq).start()+recog_nucl_index], working_seq[rec_seq.search(working_seq).start()+recog_nucl_index:len(sequence)]+working_seq[0:rec_seq.search(working_seq).start()+recog_nucl_index],'linear'
+                return working_seq[rec_seq.search(working_seq).start()+recog_nucl_index:len(sequence)]+working_seq[0:rec_seq.search(working_seq).start()+recog_nucl_index], working_seq[rec_seq.search(working_seq).start()+recog_nucl_index:len(sequence)]+working_seq[0:rec_seq.search(working_seq).start()+recog_nucl_index],'linear',rec_seq.search(working_seq).start()+recog_nucl_index
             else:
-                return sequence,'END OF SEQUENCE','circular'
+                return sequence,'END OF SEQUENCE','circular',''
         if match_start is not None and (len(rec_seq.findall(str(sequence)))!=1 or status!='circular'):
-            return sequence[:rec_seq.search(sequence).start()+recog_nucl_index],sequence[recog_nucl_index+rec_seq.search(sequence).start():],'linear'
+            return sequence[:rec_seq.search(sequence).start()+recog_nucl_index],sequence[recog_nucl_index+rec_seq.search(sequence).start():],'linear',rec_seq.search(sequence).start()+recog_nucl_index
         else:
-            return sequence,'END OF SEQUENCE','linear'
+            return sequence,'END OF SEQUENCE','linear',''
 
     def string_processor(self, old_fragment_list,recognition,recog_nucl_index,status):
         new_fragment_list = []
+        line_marker_list = []
+        line_marker_init = 0
         for fragment in old_fragment_list:
             seq1 = fragment
             seq2 = ''
             while seq2!='END OF SEQUENCE':
                 if seq1!=fragment and seq2!=seq1:
                     new_fragment_list+=[seq1]
+                    line_marker_list+=[int(line_marker)+int(line_marker_init)]
+                    line_marker_init+=int(line_marker)
                     seq1 = seq2
-                seq1,seq2,status=self.string_cutter(seq1,recognition,recog_nucl_index,status)
+                seq1,seq2,status,line_marker=self.string_cutter(seq1,recognition,recog_nucl_index,status)
             if seq2 == 'END OF SEQUENCE':
                 new_fragment_list+=[seq1]
         if len(new_fragment_list) in [1,2]  and len(old_fragment_list)==1:
             seq1 = old_fragment_list[0]*2
             store = old_fragment_list[0]
             seq2 = ''
-            seq1,seq2,status = self.string_cutter(seq1,recognition,recog_nucl_index,status)
+            seq1,seq2,status,line_marker = self.string_cutter(seq1,recognition,recog_nucl_index,status)
             single_cleavage_sequence = seq2+seq1[0:(len(store)-len(seq2))]
-        return new_fragment_list,status
+        return new_fragment_list,status,line_marker_list
 
     #def traverse_circular_loop(sequence,recog_length,recog,recog_nucl_index,status):
         #rec_seq = re.compile(recognition)
@@ -152,10 +157,14 @@ class Dnadigest():
             fragment_list = [seq]
             recognition = [enzyme_dict[enzyme][0][0] for enzyme in cut_with]
             recog_nucl_index = [enzyme_dict[enzyme][0][2] for enzyme in cut_with]
+            q = 0
             for recognition_pair in zip(recognition,recog_nucl_index):
-                fragment_list,status = self.string_processor(fragment_list,recognition_pair[0],recognition_pair[1],status)
+                fragment_list,status,line_marker_list = self.string_processor(fragment_list,recognition_pair[0],recognition_pair[1],status)
+                assoc_enzyme_list = [cut_with[q] for mark in line_marker_list]
+                q+=1
             if '' in fragment_list:
                 fragment_list.remove('')
-            print seq, fragment_list
+            return fragment_list,assoc_enzyme_list,line_marker_list,len(seq)
+            
 
 
--- tests/test_dnadigest.py
+++ tests/test_dnadigest.py
@@ -7,4 +7,22 @@ class TestDnaDigest(unittest.TestCase):
         dd = dnadigest.Dnadigest()
         enzyme_dict = dd.get_dict('enzyme_data.yaml')
         assert True
+    def test_no_restriction_site(self):
+        dd = dnadigest.Dnadigest()
+        enzyme_dict = dd.get_dict('enzyme_data.yaml')
+    def test_one_restriction_site(self):
+        dd = dnadigest.Dnadigest()
+        enzyme_dict = dd.get_dict('enzyme_data.yaml')
+        break
+    def test_multi_restriction_sites(self):
+        dd = dnadigest.Dnadigest()
+        enzyme_dict = dd.get_dict('enzyme_data.yaml')
+        break
+    def test_one_site_requiring_loop(self):
+        dd = dnadigest.Dnadigest()
+        enzyme_dict = dd.get_dict('enzyme_data.yaml')
+        break
+    def test_multi_sites_requiring_loop(self):
+        dd = dnadigest.Dnadigest()
+        enzyme_dict = dd.get_dict('enzyme_data.yaml')
 
