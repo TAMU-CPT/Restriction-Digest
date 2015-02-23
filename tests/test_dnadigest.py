d998cecde539a9769d0196f3350d9ccc48ee68be
Eric Rasche <rasche.eric@yandex.ru>
2015-02-23 14:03:32 -0600

    Test for new method

--- tests/test_dnadigest.py
+++ tests/test_dnadigest.py
@@ -3,26 +3,7 @@ import dnadigest
 
 class TestDnaDigest(unittest.TestCase):
 
-    def test_process_data_no_enzymes(self):
-        dd = dnadigest.Dnadigest()
-        enzyme_dict = dd.get_dict('enzyme_data.yaml')
-        assert True
-    def test_no_restriction_site(self):
-        dd = dnadigest.Dnadigest()
-        enzyme_dict = dd.get_dict('enzyme_data.yaml')
-    def test_one_restriction_site(self):
-        dd = dnadigest.Dnadigest()
-        enzyme_dict = dd.get_dict('enzyme_data.yaml')
-        break
-    def test_multi_restriction_sites(self):
-        dd = dnadigest.Dnadigest()
-        enzyme_dict = dd.get_dict('enzyme_data.yaml')
-        break
-    def test_one_site_requiring_loop(self):
-        dd = dnadigest.Dnadigest()
-        enzyme_dict = dd.get_dict('enzyme_data.yaml')
-        break
-    def test_multi_sites_requiring_loop(self):
-        dd = dnadigest.Dnadigest()
-        enzyme_dict = dd.get_dict('enzyme_data.yaml')
 
+    def test_expansion(self):
+        dd = dnadigest.Dnadigest()
+        self.assertEqual(dd.expand_multiple('A3N2B9'), 'AAANNBBBBBBBBB')
