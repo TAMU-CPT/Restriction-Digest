831d9098d7ee45d242d3575659543b48e7f88f95
Eric Rasche <rasche.eric@yandex.ru>
2015-03-16 09:37:59 -0500

    Updates to xml files

--- bin/graphic_drawer.py
+++ bin/graphic_drawer.py
@@ -13,15 +13,24 @@ if __name__ == '__main__':
     # A single string with default value of 'enzyme_data.yaml'
     parser.add_argument('--data', help='Enzyme cut site dataset',
                         default='enzyme_data.yaml')
+    parser.add_argument('--standalone', action="store_true", help='Produces a standalone graphic, rather than full HTML page. Only useful with one sequence in a fasta file')
     # A list of one or more strings, at the end
     parser.add_argument('enzyme', help='Comma separated list of enzymes')
     args = parser.parse_args()
 
     dd = dnadigest.Dnadigest()
 
-    print '<html><head></head><body>'
+    if not args.standalone:
+        print '<html><head></head><body>'
     for record in SeqIO.parse(args.file, 'fasta'):
         cut_sites = dd.find_cut_sites(str(record.seq), args.enzyme.split(','))
-        print '<h1>%s</h1>' % record.id
+        if not args.standalone:
+            print '<h1>%s</h1>' % record.id
         print dd.drawer(len(record.seq), cut_sites, sequence_id=record.id)
-    print '</body></html>'
+
+        # Exit after only one sequence (svg spec)
+        if args.standalone:
+            break
+
+    if not args.standalone:
+        print '</body></html>'
