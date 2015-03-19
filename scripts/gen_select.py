f40c475c2c118f0a4fbf2a6f98194d24f9b3f33e
Eric Rasche <rasche.eric@yandex.ru>
2015-03-19 11:33:54 -0500

    Switch to old style of opts with new select2 in galaxy

--- scripts/gen_select.py
+++ scripts/gen_select.py
@@ -5,11 +5,8 @@ import sys
 with open(sys.argv[1], 'r') as handle:
     data = yaml.load(handle)
 
-full_list = []
 for x in data:
-    for q in ([data[x]['enzyme']] + data[x]['isoscizomers']):
-        if q not in full_list:
-            full_list.append(q)
-
-for q in sorted(full_list):
-    print '<option value="%s">%s</option>' % (q, q)
+    if len(data[x]['isoscizomers']) > 0:
+        print '<option value="%s">%s [%s]</option>' % (data[x]['enzyme'], data[x]['enzyme'], ', '.join( data[x]['isoscizomers']))
+    else:
+        print '<option value="%s">%s</option>' % (data[x]['enzyme'], data[x]['enzyme'])
