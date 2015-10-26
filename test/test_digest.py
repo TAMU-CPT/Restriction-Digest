57d2c133c166b9379943b07c4113d3ca29eacd9a
Eric Rasche <rasche.eric@gmail.com>
2015-10-26 16:41:59 -0500

    More test cases moved over

--- test/test_digest.py
+++ test/test_digest.py
@@ -1,23 +1,49 @@
+#!/usr/bin/env python
+"""Test cases for DNA Digest library"""
 import unittest
 import os
 import dnadigest as dd
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 
 class TestDnaDigest(unittest.TestCase):
+    """Test DnaDigest classes"""
 
     def test_enzyme(self):
-        e = dd.Enzyme(name='EcoRI', forward=('G', 'A2T2C'), reverse=('CTTAA', 'G'))
-        self.assertEqual(e.expand_multiple('A3N2B9'), 'AAANNBBBBBBBBB')
-        fr, rv = e.get_regex()
-        self.assertTrue(fr.match('GAATTC'))
-        self.assertFalse(rv.match('GAATTC'))
+        """test enzyme class"""
+        enzyme = dd.Enzyme(name='EcoRI', forward=('G', 'A2T2C'), reverse=('CTTAA', 'G'))
+        self.assertEqual(enzyme.expand_multiple('A3N2B9'), 'AAANNBBBBBBBBB')
+        forward, reverse = enzyme.get_regex()
+        self.assertTrue(forward.match('GAATTC'))
+        self.assertFalse(reverse.match('GAATTC'))
 
-        self.assertFalse(fr.match('CTTAAG'))
-        self.assertTrue(rv.match('CTTAAG'))
+        self.assertFalse(forward.match('CTTAAG'))
+        self.assertTrue(reverse.match('CTTAAG'))
 
-    def test_enzymelibrary(self):
+        self.assertEqual(enzyme.iupac_to_regex('KAYAK'),
+                         '[GT]A[CT]A[GT]')
+
+    def test_dnadigest(self):
+        """test dnadigest"""
         bsp = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                            'bsp.yaml')
-        el = dd.EnzymeLibrary(bsp)
-        print el
-        # sequence = 'qqqCGnCGwwwwwwTCCGGAeeeeeAGGCCTrrrrrGACNNNNNNGTCoooGCnGCooo'
+        digester = dd.DnaDigest(bsp)
+        sequence = SeqRecord(
+            Seq('nnnCGnCGnnnnnnTCCGGAnnnnnAGGCCTnnnnnGACNNNNNNGTCnnnGCnGCnnn'), id='test')
+        fragments, cut_sites, did_cut = digester.digest_sequence(sequence, ['Bsp6I'])
+        self.assertTrue(did_cut)
+        self.assertDictEqual(
+            {65: ['Bsp6I']},
+            cut_sites
+        )
+
+        self.assertEqual(
+            'nGCnnnnnnCGn',
+            str(fragments[0].seq)
+        )
+
+        self.assertEqual(
+            'CGnnnnnnTCCGGAnnnnnAGGCCTnnnnnGACNNNNNNGTCnnnGC',
+            str(fragments[1].seq)
+        )
